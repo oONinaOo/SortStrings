2c7367fee37f4dd3db45f7c19ce41eba357bfa5e
oONinaOo <loveninagold@gmail.com>
2017-06-06 07:27:03 +0200

    finished SortString

---
+++ SortString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Nos ide most beírtam valamit, hogy menjen a hely bla bla bla.";
+            string[] sa = s.Split(' ');
+
+            Array.Sort(sa);
+
+            s = string.Join(" ", sa);
+            Console.WriteLine(s);
+        }
+    }
+}
